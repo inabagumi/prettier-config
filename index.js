c5ca43fc9c68d822dab4cf99791dd40e6272d91d
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-03-11 22:45:16 +0900

    feat!: change to esm (#295)

---
+++ index.js
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const prettierConfig = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+}
+
+export default prettierConfig
